89d756cb540cfb80d1ca67cb08e62810c88342b8
Andreas Gohr <andi@splitbrain.org>
2019-05-25 08:23:45 +0200

    added templates for all three types

--- TemplateController.php
+++ TemplateController.php
@@ -2,6 +2,13 @@
 
 namespace dokuwiki\template\twigstarter;
 
+use BadFunctionCallException;
+use BadMethodCallException;
+use dokuwiki\Menu\MenuInterface;
+use Exception;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 class TemplateController
@@ -28,9 +35,10 @@ class TemplateController
         if (is_dir(tpl_basedir() . '/templates')) {
             $paths[] = tpl_basedir() . '/templates';
         }
-        $loader = new \Twig\Loader\FilesystemLoader($paths);
-        $this->twig = new \Twig\Environment($loader, [
-            'cache' => $conf['cachedir'],
+        $loader = new FilesystemLoader($paths);
+        $this->twig = new Environment($loader, [
+            'cache' => $conf['allowdebug'] ? false : $conf['cachedir'],
+            'debug' => $conf['allowdebug'],
         ]);
 
     }
@@ -46,6 +54,7 @@ class TemplateController
 
         // register all globals to be available in twig
         $data = $GLOBALS;
+        $data['_SERVER'] = $_SERVER;
 
         // make this controller available in twig as TPL
         $data['TPL'] = $this;
@@ -56,7 +65,7 @@ class TemplateController
         // render the current view template
         try {
             echo $this->twig->render($this->view . '.twig', $data);
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             $msg = hsc($e->getMessage());
             if ($conf['allowdebug']) {
                 $msg .= '<pre>' . hsc($e->getTraceAsString()) . '</pre>';
@@ -67,9 +76,34 @@ class TemplateController
     }
 
     /**
+     * Initializes and returns one of the menus
+     *
+     * @param string $type
+     * @return MenuInterface
+     */
+    public function menu($type)
+    {
+        $class = '\\dokuwiki\\Menu\\' . ucfirst($type) . 'Menu';
+        if (class_exists($class)) {
+            return new $class();
+        }
+
+        throw new BadMethodCallException("No such menu $type");
+    }
+
+    /**
+     * Return the current view as set in the constructor
+     *
+     * @return string
+     */
+    public function getView() {
+        return $this->view;
+    }
+
+    /**
      * We make all our functions available to the template as methods of this object
      *
-     * We always need the function to return their data, not print it so we use output buffering to
+     * We always need the functions to return their data, not print it so we use output buffering to
      * catch any possible output.
      *
      * @param string $name
@@ -88,7 +122,7 @@ class TemplateController
             return $return;
         }
 
-        throw new \BadFunctionCallException("Function $name() does not exist");
+        throw new BadFunctionCallException("Function $name() does not exist");
     }
 
 
